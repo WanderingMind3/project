0bfc89c2a90e5a55fbceb6d32a591a970521d9da
Abhishek <iabhishek.4283@gmail.com>
2023-05-02 15:07:38 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+import React from "react"
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+
+        <input type="text" name="FirstName" placeholder="First Name" />
+        <input type="text" name="LastName" placeholder="Last Name" />
+        <div>
+
+          <input type="radio" id="active" name="status" value="active" checked={true} />
+          <label htmlFor="active">Active</label>
+
+          <input type="radio" id="inactive" name="status" value="inactive" />
+          <label htmlFor="inactive">Inactive</label>
+        </div>
+
+        <button>Save Contact</button>
+
+      </div>
+
+
+
+    </>
+  )
+}
+
+export default App
