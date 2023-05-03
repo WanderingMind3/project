7154f9e2114a296de251c6c7140ec0f096600e58
Priyanshu <priyanshu.s1619@gmail.com>
2023-05-03 13:07:48 +0530

    fixed lastName issue

--- src/form.tsx
+++ src/form.tsx
@@ -8,22 +8,31 @@ interface NewFormProps{
 export const Form:React.FC<NewFormProps> = ({ saveContacts }) => {
 
     const [contact, setContact] = React.useState("")
+    const [lastNameContact, setLastNameContact] = React.useState("")
+
 
     const updateContact = (event:ChangeEvent<HTMLInputElement>) => {
         setContact(event.target.value)
     }
+    const updateLastNameContact = (event:ChangeEvent<HTMLInputElement>) => {
+        setLastNameContact(event.target.value)
+    }
 
     const onSaveContactClick = () => {
         saveContacts(contact)
+        saveContacts(lastNameContact)
         setContact("")
+        setLastNameContact("")
+
     }
+   
 
     return(
         <>
         <div>
 
             <input onChange={updateContact} value={contact} type="text" name="FirstName" placeholder="First Name" />
-            <input  type="text" name="LastName" placeholder="Last Name" />
+            <input onChange={updateLastNameContact} value={lastNameContact} type="text" name="LastName" placeholder="Last Name" />
             <div>
 
                 <input  type="radio" id="active" name="status" value="active"/>
